f39f191ded94cf55321b2cb8b3dbcd4a05a9e534
Tyler Graf <tylergraf@gmail.com>
2024-09-23 15:38:07 -0600

    optimistic

--- app/components/Todos.tsx
+++ app/components/Todos.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useOptimistic, useTransition} from 'react';
 import type { Todo } from '@prisma/client'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,35 +15,45 @@ const Todos: React.FC<TodosProps> = () => {
   const [title, setTitle] = useState('');
   const [{ data: todos }, refetchTodos] = useAxios<Todo[]>('/api/todos')
 
-  const [{ loading: addTodoLoading }, addTodo] = useAxios<Todo>(
+  const [isPending, startTransition] = useTransition()
+  const [optimisticTodos, setOptimisticTodos] = useOptimistic(todos || []);
+
+  const [, addTodo] = useAxios<Todo>(
     { url: '/api/todo/new', method: 'POST' },
     { manual: true }
   );
   
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
-    const newTodo = {
-      title: formData.get('title') as string,
-      completed: false,
-    };
+    
+    startTransition(async () => {
 
-    try {
-      await addTodo({ data: newTodo });
-      setTitle('')
-      refetchTodos();
-    } catch (err) {
-      toast('Failed to add todo', { type: 'error' });
-    }
+      const formData = new FormData(e.target as HTMLFormElement);
+      const title = formData.get('title') as string;
+      const newTodo = {
+        title,
+        completed: false,
+      };
+      
+      setOptimisticTodos((oldTodos) => [...oldTodos, { id: Math.random(), title, completed: false }]);
+      
+      try {
+        await addTodo({ data: newTodo });
+        setTitle('')
+        refetchTodos();
+      } catch (err) {
+        toast('Failed to add todo', { type: 'error' });
+      }
+    });
   }
   
   return (
     <div className='container mx-auto max-w-96 mt-16'>
       <ToastContainer></ToastContainer>
       <h1 className='text-2xl font-bold mb-3'>Todos</h1>
-      <NewTodoForm handleSubmit={handleSubmit} title={title} setTitle={setTitle} loading={addTodoLoading}></NewTodoForm>
+      <NewTodoForm handleSubmit={handleSubmit} title={title} setTitle={setTitle} loading={isPending}></NewTodoForm>
       <ul>
-        {todos?.map(todo => (
+        {optimisticTodos?.map(todo => (
           <li key={todo.id} className='flex flex-row justify-between mb-1 border-b items-center py-1'>
             <span>{todo.title}</span>
             <DeleteTodoButton todoId={todo.id} refetchTodos={refetchTodos}></DeleteTodoButton>
